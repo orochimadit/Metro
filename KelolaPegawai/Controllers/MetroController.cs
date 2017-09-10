a0845edcb5f76d72c407242d070da521e5adfd2b
Aditya Pratama <adityapratamabadra@gmail.com>
2017-09-09 23:07:02 -0700

    MVC dengan metro

--- KelolaPegawai/Controllers/MetroController.cs
+++ KelolaPegawai/Controllers/MetroController.cs
@@ -33,5 +33,22 @@ namespace KelolaPegawai.Controllers
         {
             return View();
         }
+        public ActionResult Latihan()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Latihan(string TextBoxNama, string TextAreaAlamat, string DropDownListPropinsi, string RadioButtonJenisKelamin, bool CheckBoxBahasaCSharp, bool CheckBoxBahasaJava, bool CheckBoxBahasaPhyton, IEnumerable<string> ListBoxPekerjaan)
+        {
+            string languages = String.Empty; string jobs = String.Empty;
+            if (CheckBoxBahasaCSharp) { languages = "C#" + " " + languages; }
+            if (CheckBoxBahasaJava) { languages = "Java" + " " + languages; }
+
+
+            if (CheckBoxBahasaPhyton) { languages = "Phyton" + " " + languages; }
+            foreach (string job in ListBoxPekerjaan) { if (job.Equals("1")) { jobs = "Programmer " + jobs; } else if (job.Equals("2")) { jobs = "Analis Sistem " + jobs; } else if (job.Equals("3")) { jobs = "Administrator Database " + jobs; } else { jobs = "Manajer Proyek " + jobs; } }
+            ViewBag.Hasil = TextBoxNama + " " + TextAreaAlamat + " " + DropDownListPropinsi + " " + RadioButtonJenisKelamin + " " + languages + " " + jobs; return View();
+        }
+        
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Metro
+# Metro
